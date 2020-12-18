2827b84ae6b30af4d99bdddfa09818b49aa68f79
Bayraktar <alihan.bayraktar@siemens.com>
2020-12-18 14:23:25 +0300

    examples_04 eklendi

---
+++ examples_04/Odev.Test/OperationTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Odev.Test
+{
+    [TestFixture]
+    public class OperationTest
+    {
+     
+
+
+        [Test]
+        [TestCase(10, 20, '+', 30)]
+        [TestCase(10, 20, '*', 200)]
+        [TestCase(10, 20, '-', -10)]
+        [TestCase(10, 20, '/', 0.5)]
+        public async Task Apply_GiveArguments_TrueResult(int num1, int num2, char operationSysmbol, double expected)
+        {
+            Operation operation = OperationFactory.Create(num1, num2, operationSysmbol);
+
+            double actual =  await operation.ApplyAsync();
+
+            
+            Assert.AreEqual(expected, actual);
+        
+        }
+
+        public void Aaa()
+        {
+            Operation operation = OperationFactory.Create(1, 1, '+');
+
+            Task<double> r = operation.ApplyAsync();
+        }
+
+        
+    }
+}
---
+++ examples_04/Odev/TwoOperation.cs
@@ -0,0 +1,18 @@
+﻿namespace Odev
+{
+    public abstract class TwoOperation : Operation
+    {
+        protected double param1;
+        protected double param2;
+
+        public TwoOperation(double param1, double param2)
+        {
+            this.param1 = param1;
+            this.param2 = param2;
+        }
+
+        public abstract double Apply();
+    }
+
+
+}
