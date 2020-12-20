a8ee58561f4d820a9c87d950134a9a0d795dba8e
bayrktlihn <bayraktaralihan1995@gmail.com>
2020-12-21 00:09:12 +0300

    ten finger oyunu eklendi

---
+++ examples_05/TenFinger/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace TenFinger
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wordTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.currentWordLabel = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // wordTextBox
+            // 
+            this.wordTextBox.Enabled = false;
+            this.wordTextBox.Location = new System.Drawing.Point(12, 34);
+            this.wordTextBox.Name = "wordTextBox";
+            this.wordTextBox.Size = new System.Drawing.Size(100, 22);
+            this.wordTextBox.TabIndex = 0;
+            this.wordTextBox.TextChanged += new System.EventHandler(this.wordTextBox_TextChanged);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(137, 27);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(94, 36);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Başlat";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // currentWordLabel
+            // 
+            this.currentWordLabel.AutoSize = true;
+            this.currentWordLabel.Location = new System.Drawing.Point(12, 9);
+            this.currentWordLabel.Name = "currentWordLabel";
+            this.currentWordLabel.Size = new System.Drawing.Size(0, 17);
+            this.currentWordLabel.TabIndex = 2;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(134, 7);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(0, 17);
+            this.timeLabel.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 86);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.currentWordLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.wordTextBox);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox wordTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label currentWordLabel;
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
---
+++ examples_05/TenFinger/MainForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TenFinger
+{
+    public partial class MainForm : Form
+    {
+        private Queue<String> _words = new Queue<string>();
+        private int _corretWordCount;
+        private int _wrongWordCount;
+
+
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            LoadWords();
+        }
+
+        private void Reset()
+        {
+            _corretWordCount = 0;
+            _wrongWordCount = 0;
+
+            LoadWords();
+
+            wordTextBox.Enabled = true;
+            startButton.Enabled = false;
+
+            currentWordLabel.Text = _words.Peek();
+        }
+
+        private void LoadWords()
+        {
+            _words.Clear();
+
+            String text = @"
+            the enterprise centre has an important role in the pathogenesis of acute pancreatitis in fine form and the consequent conservative government since 1979 the government has published a document in the new system will be installed at the company's headquarters in new york and scarborough and filey of regent's park, london and basingstoke, macmillan conference, at the very last minute en equally unsuccessfully competent in the use of the mental element in the `new species and nancial problems are also available for the taking.
+            ".Trim().ClearPunctuation();
+
+            String[] words = text.Split(' ');
+
+
+            words = ShuffleWords(words);
+
+
+
+            foreach (String word in words)
+            {
+                _words.Enqueue(word);
+            }
+
+        }
+
+        private String[] ShuffleWords(string[] words)
+        {
+            Random random = new Random();
+            String [] result = words.OrderBy(p => random.NextDouble()).ToArray();
+
+            return result;
+        }
+
+        
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            Reset();
+
+
+            Thread t = new Thread(() => {
+                for (int i = 0; i < 60; i++)
+                {
+                    Thread.Sleep(1000);
+                    timeLabel.Text = "" + (i + 1);
+
+
+                }
+
+                wordTextBox.Enabled = false;
+                startButton.Enabled = true;
+
+                currentWordLabel.Text = "";
+                timeLabel.Text = "";
+
+                wordTextBox.Text = "";
+                MessageBox.Show($"Correct Word Count: {_corretWordCount}, Wrond Word Count: {_wrongWordCount}", "Information");
+            });
+
+            t.IsBackground = true;
+
+            t.Start();
+
+        }
+
+
+
+        private void wordTextBox_TextChanged(object sender, EventArgs e)
+        {
+            String enteredWord = wordTextBox.Text;
+
+
+            if (enteredWord != null && enteredWord != "")
+            {
+
+                if (enteredWord[enteredWord.Length - 1] == ' ')
+                {
+                    enteredWord = enteredWord.Trim();
+                    String currentWord = _words.Dequeue();
+
+
+                    if (currentWord == enteredWord) {
+                        _corretWordCount++;
+                    }
+                    else
+                    {
+                        _wrongWordCount++;
+                    }
+
+                    wordTextBox.Text = "";
+
+                    currentWordLabel.Text = _words.Peek();
+
+                }
+
+            }
+        }
+    }
+
+    public static class MyStringExtensionMethods{
+        public static String ClearPunctuation(this String word)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char letter in word)
+            {
+                if (!char.IsPunctuation(letter))
+                {
+                    sb.Append(letter);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
